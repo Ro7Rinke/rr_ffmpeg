0843eaa45c7d0fd56fe8044ba10db4bc80e5e981
Ro7Rinke <ro7rinke@gmail.com>
2023-08-28 23:04:53 -0300

    version 1.0 release

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const Ffmpeg = require('fluent-ffmpeg')
 
 // let command = ffmpeg('my-file.mp4')
 
-
-
 // Ffmpeg.getAvailableFormats(function(err, formats) {
 //   console.log('Available formats:');
 //   console.dir(formats);
@@ -36,50 +34,101 @@ const Ffmpeg = require('fluent-ffmpeg')
 // new Ffmpeg({ source: '/path/to/file.avi' })
 //   .getAvailableCodecs(...);
 
-const pathInputs = 'C:/ff_mpeg/input'
-const pathOutputs = 'C:/ff_mpeg/output'
+const args = process.argv.slice(2);
+
+const pathInputs = args[0] ?? './input'
+const pathOutputs = args[1] ?? './output'
+
+const videoCodec = args[2] ?? 'hevc_nvenc' // 'h264_nvenc'
+
+const asyncConvert = args[3] ?? false
 
 const getInputFiles = () => {
-  const paths = fs.readdirSync(pathInputs)
-  let files = []
+    const paths = fs.readdirSync(pathInputs)
+    let files = []
 
-  for (const path of paths) {
-    const data = path.split('.')
-     
-    files.push({
-      name: data[0],
-      type: data[1]
-    })  
-  }
+    for (const path of paths) {
+        const data = path.split('.')
 
-  return files
+        files.push({
+            name: data[0],
+            type: data[1]
+        })
+    }
+
+    return files
 }
 
 const convert = async (file) => {
-
-  let command = Ffmpeg(`${pathInputs}/${file.name}.${file.type}`)
-    .videoCodec('hevc_nvenc')
-    .on('progress', function(progress) {
-      console.log(`Processing ${file.name}: ${progress.percent.toFixed(2)} % done`)
+    return new Promise((resolve, reject) => {
+        let command = Ffmpeg(`${pathInputs}/${file.name}.${file.type}`)
+            .videoCodec(videoCodec)
+            .on('progress', function (progress) {
+                console.log(`Processing ${file.name}: ${progress.percent.toFixed(2)} % done`)
+            })
+            .on('error', function (err, stdout, stderr) {
+                console.error(`Cannot process ${file.name}: ${err.message}`)
+                reject(err)
+            })
+            .on('end', function (stdout, stderr) {
+                console.log(`Transcoding ${file.name} succeeded!`)
+                resolve()
+            })
+            .save(`${pathOutputs}/${file.name}.mp4`)
     })
-    .on('error', function(err, stdout, stderr) {
-      console.log(`Cannot process ${file.name}: ${err.message}`)
-    })
-    .on('end', function(stdout, stderr) {
-      console.log(`Transcoding ${file.name} succeeded!`)
-    })
-
-    await command.save(`${pathOutputs}/${file.name}.mp4`)
 
 }
 
-const main = async () =>{
-  const files = getInputFiles()
-  for (const file of files) {
-    await convert(file)
-  }
+const processFiles = async () => {
+    return new Promise(async (resolve, reject) => {
+        if(fs.existsSync(pathInputs)){
+            if(!fs.statSync(pathInputs).isDirectory()){
+                console.error(`The specified path "${pathInputs}" is not a directory!`)
+                return
+            }
+        }else{
+            console.error(`The specified path "${pathInputs}" does not exist!`)
+            return
+        }
+    
+        if(fs.existsSync(pathOutputs)){
+            if(!fs.statSync(pathOutputs).isDirectory()){
+                console.error(`The specified path "${pathOutputs}" is not a directory!`)
+                return
+            }
+        }else{
+            fs.mkdirSync(pathOutputs)
+        }
+    
+        let promises = []
+
+        const files = getInputFiles()
+        for (const file of files) {
+            asyncConvert ? promises.push(convert(file)) : await convert(file)
+        }
+
+        if(asyncConvert){
+            Promise.all(promises)
+                .then(data => {
+                    resolve()
+                })
+                .catch(error => {
+                    reject(error)
+                })
+        }else{
+            resolve()
+        }
+    })
 }
 
+const main = async () => {
+    const startTime = new Date()
+    
+    await processFiles()
+    
+    const endTime = new Date
 
+    console.log(`Time elapsed: ${endTime - startTime} ms`)
+}
 
 main()
